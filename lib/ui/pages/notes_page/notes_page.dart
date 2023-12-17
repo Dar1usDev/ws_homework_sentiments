cae20872fe980f58c92e8a51229bbde4fd508868
Dmitry <66305223+Dar1usDev@users.noreply.github.com>
2023-12-17 19:44:37 +1000

    add: temp x2

---
+++ lib/ui/pages/notes_page/notes_page.dart
@@ -0,0 +1,18 @@
+import 'package:flutter/material.dart';
+import 'package:ws_homework_sentiments/ui/pages/notes_page/widgets/notes_scrollview.dart';
+
+class NotesPage extends StatefulWidget {
+  const NotesPage({super.key});
+
+  @override
+  State<NotesPage> createState() => _NotesPageState();
+}
+
+class _NotesPageState extends State<NotesPage> {
+  @override
+  Widget build(BuildContext context) {
+    return const Scaffold(
+      body: NotesScrollView(),
+    );
+  }
+}
